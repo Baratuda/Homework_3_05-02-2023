c435fad76266d9ce82e833285a4a8972df4b0c66
Baratuda <104141434+Baratuda@users.noreply.github.com>
2023-02-05 19:10:39 +0300

    Update Task2.cs

--- Task2.cs
+++ Task2.cs
@@ -1,17 +1,16 @@
-﻿
-    Console.Clear();
-    Console.Write("Please enter the number: ");
-    int x=int.Parse(Console.ReadLine());
-    int y=1;
-    int m=x;
+Console.Clear();
+Console.Write("Please enter the number: ");
+int x=int.Parse(Console.ReadLine());
+int y=1;
+int m=x;
 
-    if(x<0){ y=x; m=-x;  x=-1;}  
+if(x<0){ y=x; m=-x;  x=-1;}  
     
-    double[] nums = new double[m];
-    for(int i=1; y<=x; y++, i++){
-      nums[i-1] = Math.Pow(y, 3); 
-    } 
-    Console.Write(String.Join(", ", nums));
+double[] nums = new double[m];
+for(int i=1; y<=x; y++, i++){
+   nums[i-1] = Math.Pow(y, 3); 
+} 
+Console.Write(String.Join(", ", nums));
   
 
 
